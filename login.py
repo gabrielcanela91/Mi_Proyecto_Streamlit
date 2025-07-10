b3afe435153836f105ce14719b2b62a4952b930b
gabrielcanela91 <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-10 22:09:35 +0000

    Actualizaacion

--- login.py
+++ login.py
@@ -156,7 +156,7 @@ def menu_principal():
     #---------------Estilo del titulo con HTMLy CSS------
     st.markdown("""
         <div style='position: relative; top: 0; left: 0; padding: 10px 0 10px 0;'>
-            <h1 style='margin: 0; text-align: left; color: #004E66;'>📊 GRÁFICOS</h1>
+            <h1 style='margin: 0; text-align: left; color: black;'>📊 GRÁFICOS</h1>
         </div>
     """, unsafe_allow_html=True)
 
