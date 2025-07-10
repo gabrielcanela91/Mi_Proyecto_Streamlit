9ab0eff274167390f22557b8ae19ae3970d9fe94
gabrielcanela91 <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-10 23:19:05 +0000

    Actualizaacion

--- login.py
+++ login.py
@@ -121,12 +121,6 @@ def registrar_estilo_sidebar():
 
 #--------------------MENU PRINCIPAL --------------
 def menu_principal():
-        #---------------Estilo del titulo con HTMLy CSS------
-    st.markdown("""
-        <div style='position: relative; top: 0; left: 0; padding: 10px 0 10px 0;'>
-            <h1 style='margin: 0; text-align: left; color: #0a7fa3;'>📊 GRÁFICOS</h1>
-        </div>
-    """, unsafe_allow_html=True)
 
     registrar_estilo_sidebar()
     st.set_page_config(initial_sidebar_state="expanded")
@@ -160,6 +154,13 @@ def menu_principal():
     if st.sidebar.button("Registros"):
         st.session_state["paso_actual"] = 5
         st.rerun()
+        
+            #---------------Estilo del titulo con HTMLy CSS------
+    st.markdown("""
+        <div style='position: relative; top: 0; left: 0; padding: 10px 0 10px 0;'>
+            <h1 style='margin: 0; text-align: left; color: #0a7fa3;'>📊 GRÁFICOS</h1>
+        </div>
+    """, unsafe_allow_html=True)
 
 
 # ---------------- PESTAÑA 1: INTRODUCCIÓN ----------------
