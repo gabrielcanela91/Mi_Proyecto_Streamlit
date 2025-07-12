c0aa8b78c663c7c1298b31204912ac7dbd93ea8b
gabrielcanela91 <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-12 01:29:21 +0000

    actualizacion

--- login.py
+++ login.py
@@ -153,7 +153,7 @@ def menu_principal():
         <img src="{url_imagen}" class="circle-img">
     """, unsafe_allow_html=True)
 
-    st.sidebar.markdown("<h2 style='text-align:center;'>🏠 Menú Principal</h2>", unsafe_allow_html=True)
+    st.sidebar.markdown("<h2 style='text-align:center;'>🏠 Men Principal</h2>", unsafe_allow_html=True)
     
             
         # Botón funcional que te lleva directamente al paso 1 (índice 1 = Bienvenida)
