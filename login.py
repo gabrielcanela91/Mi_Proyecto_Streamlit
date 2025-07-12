7164c177ce7ee97a204bca2e38b8545410054a53
gabrielcanela91 <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-12 02:26:40 +0000

    actualizacion

--- login.py
+++ login.py
@@ -127,7 +127,7 @@ def menu_principal():
     st.markdown("""
     <style>
         .block-container {
-            padding-top: 0rem !important;
+            padding-top: 5rem !important;
         }
     </style>
 """, unsafe_allow_html=True)
