1b35925217678f61153502f8e743e7c6bb1bacd5
gabrielcanela91 <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-12 02:35:48 +0000

    actualizacion

--- login.py
+++ login.py
@@ -125,16 +125,16 @@ def registrar_estilo_sidebar():
 def menu_principal():
 
     st.markdown("""
-    <style>
-        .block-container {
-            padding-top: 0rem !important;
-        }
-    </style>
-""", unsafe_allow_html=True)
+        <style>
+            .block-container {
+                padding-top: 0rem !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
 
  #---------------Estilo del titulo con HTMLy CSS------
     st.markdown("""
-        <div style='position: fixed; top: 0; left: 0; width: calc(100% - 250px); margin - left: 300px; padding: 10px 0 10px 0;'>
+        <div style='position: fixed; top: 0; left: 0; width: calc(100% - 250px); margin-left: 300px; padding: 10px 0 10px 0;'>
             <h1 style='margin: 0; text-align: left; color: #0a7fa3;'>📊 GRÁFICOS</h1>
         </div>
     """, unsafe_allow_html=True)
