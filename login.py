4e5ca05c380f496838f45bb33cc7068c5b3c386f
Gabriel Canela <156148135+gabrielcanela91@users.noreply.github.com>
2025-07-15 20:02:22 +0000

    Nueva actualuzacion 150720250402

--- login.py
+++ login.py
@@ -141,7 +141,7 @@ def menu_principal():
     st.set_page_config(initial_sidebar_state="expanded")
 
     # Imagen redonda en el sidebar
-    url_imagen = "https://raw.githubusercontent.com/gabrielcanela91/Mi_Proyecto_Streamlit/refs/heads/main/Data/capacitacion.png?token=GHSAT0AAAAAADGENRXVNAHLUSJ36WS7YYMC2DWV37Q"
+    url_imagen = "https://raw.githubusercontent.com/gabrielcanela91/Mi_Proyecto_Streamlit/refs/heads/main/Data/capacitacion.png"
     st.sidebar.markdown(f"""
         <style>
             .circle-img {{
